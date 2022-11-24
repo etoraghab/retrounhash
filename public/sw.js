587da5bf721ac5ccf418b2774ffeac4f04b1e2aa
atordvairn <atordvairn@gmail.com>
2022-11-25 00:42:29 +0530

    1.1.151

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 self.addEventListener("install", function (event) {
-  var CACHE_NAME = "1.1.15";
+  var CACHE_NAME = "1.1.151";
   var urlsToCache = ["/"];
 
   self.addEventListener("install", function (event) {
